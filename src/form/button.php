3e7f77112919576cfbf88fc4d0db6e545d1faa98
Leonid Lezner <kontakt@lezner.de>
2025-02-15 13:48:17 +0100

    Fixing button

--- src/form/button.php
+++ src/form/button.php
@@ -1,1 +1,1 @@
-<button class="rounded-md px-5 py-1 text-color1 bg-color4 hover:bg-color5 cursor-pointer hover:outline hover:outline-1 hover:outline-color4" type="submit"><?php echo $args['caption'] ?></button>
+<button class="rounded-md px-5 py-1 text-color1 bg-gray-300 hover:bg-color2 hover:text-white  cursor-pointer hover:outline hover:outline-1 hover:outline-color4" type="submit"><?php echo $args['caption'] ?></button>
